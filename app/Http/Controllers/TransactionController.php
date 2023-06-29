652cbdd54d963bb3b87276564f1f27f974408cbc
Tima-Devoloper <martin_coffee@mail.ru>
2023-06-30 01:20:54 +0600

    1. Создал метод в контроллере для совершения транзакции 2. Добавил рут для транзакции

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * Контроллер траназакции
+ */
 class TransactionController extends Controller
 {
-    //
+    /**
+     * Совершаем транзакцию
+     *
+     * @param  \Illuminate\Http\Request      $request
+     * @param  int                           $user_id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function createTransaction(Request $request, int $user_id): JsonResponse
+    {
+
+    }
 }
