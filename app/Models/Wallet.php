49bbbecb37f2f3cfaf735b8fe01d22d28ad0e813
Tima-Devoloper <martin_coffee@mail.ru>
2023-06-30 00:19:04 +0600

    1. Создал миграцию таблицы кошелька пользователей 2. Создал модельку кошелька

---
+++ app/Models/Wallet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Модель кашелька пользователей
+ */
+class Wallet extends Model
+{
+    use HasFactory;
+}
